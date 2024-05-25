97096578deeb28bdf29503c06d3ed3fc176cc09a
Vivekyadav00 <vivekyadavvicky6@gmail.com>
2024-05-25 18:05:29 -0500

    this is first version of calculator

---
+++ calculator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+###################
+#Author: Vivek Yadav Baira
+#Date: 05/25/2024
+#Purpose: This script is developed for calculation 
+################
+
+set -x
+
+echo "Enter the first number"
+read num1
+
+echo "Enter the Second number"
+read num2
+
+sum=$((num1 + num2))
+#echo "sum of $num1 and $num2 is:$sum"
+echo "The sum of $num1 and $num2  is: $sum"
+
+
